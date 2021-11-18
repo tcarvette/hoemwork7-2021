c11ef0b28fe50b8482557ece394691c08c63663b
tcarvette <45024633+tcarvette@users.noreply.github.com>
2021-11-18 18:09:46 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -5,7 +5,29 @@ window.addEventListener("load", function() {
 
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+	var slider_value = document.querySelector("#slider").value;
+	var vol_ume = document.querySelector("#volume");
+	vol_ume.innterHTML = (slider_value + "%");
+});
 
+document.querySelector("#pause").addEventListener("clicl", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate -= 0.05;
+	console.log(video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate += 0.05;
+	console.log(video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	// tbd
+});
